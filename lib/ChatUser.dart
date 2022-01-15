20b4b289225835822b731a05fd947e6ac61f4a52
awais <awaisshahbazrock@gmail.com>
2022-01-16 02:37:55 +0500

    work

--- lib/ChatUser.dart
+++ lib/ChatUser.dart
@@ -88,12 +88,11 @@ class _ChatUserState extends State<ChatUser> {
                               ((elements.data() as Map)["receiverid"] ==
                                       (ele.data() as Map)["receiverid"] &&
                                   (elements.data() as Map)["senderid"] ==
-                                      (elements.data() as Map)["senderid"]) ||
+                                      (ele.data() as Map)["senderid"]) ||
                               ((elements.data() as Map)["receiverid"] ==
                                       (ele.data() as Map)["senderid"] &&
                                   (elements.data() as Map)["senderid"] ==
-                                      (elements.data()
-                                          as Map)["receiverid"])) ==
+                                      (ele.data() as Map)["receiverid"])) ==
                           i;
                     }).where((elements) {
                       return (elements.data() as Map)["senderid"] ==
